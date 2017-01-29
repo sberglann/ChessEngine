3a689d2d281b27634783baea395ba4fe5b76b1da
Sigurd Berglann <sberglann94@gmail.com>
2017-01-29 13:05:15 +0100

    auto formatting

--- src/main/scala-2.12/Board.scala
+++ src/main/scala-2.12/Board.scala
@@ -20,17 +20,23 @@ case class Board(position: List[Piece], info: List[Boolean], enPassantCol: Int) 
   lazy val blackKingPos = posFromPiece(Piece("BK")).head
 
   def row(i: Int) = i / 8
+
   def col(i: Int) = i % 8
+
   def validIndex(i: Int) = i >= 0 && i < 64
+
   def validRow(pos: Int, delta: Int, limit: Int) = math.abs(row(pos) - row(pos + delta)) <= limit
+
   def validCol(pos: Int, delta: Int, limit: Int) = math.abs(col(pos) - col(pos + delta)) <= limit
+
   def emptySquare(pos: Int) = position(pos).pieceType == 'E'
+
   def deletePiece(pos: Int) = this.copy(position = position.updated(pos, Piece("EE")))
 
   def positionAsCols(): List[List[Piece]] = {
     (for (i <- 0 until 8) yield {
       (for (j <- 0 until 8) yield {
-        position(j*8 + i)
+        position(j * 8 + i)
       }).toList
     }).toList
   }
@@ -42,7 +48,7 @@ case class Board(position: List[Piece], info: List[Boolean], enPassantCol: Int) 
 
   def prettyBoard = {
     (for (i <- position.indices) yield {
-      if (i % 8 == 0) s"\n ${8 - i/8} ║${prettyPieces(position(i).color.toString + position(i).pieceType.toString)}"
+      if (i % 8 == 0) s"\n ${8 - i / 8} ║${prettyPieces(position(i).color.toString + position(i).pieceType.toString)}"
       else prettyPieces(position(i).color.toString + position(i).pieceType.toString)
     }).foldLeft("")(_ + _) ++ "\n   ╚════════════════════════\n      a   b   c   d    e   f   g    h"
   }
@@ -54,17 +60,17 @@ case class Board(position: List[Piece], info: List[Boolean], enPassantCol: Int) 
 
     //Checks if castle or king is moved. If this is the case, castling cannot be done, and the info-list is changed
     val newInfo =
-      if (pos == whiteKingPos)      info.updated(2, false).updated(4, false)
-      else if (pos == blackKingPos) info.updated(3, false).updated(5, false)
-      else if (pos == 0)            info.updated(3, false)
-      else if (pos == 7)            info.updated(5, false)
-      else if (pos == 56)           info.updated(2, false)
-      else if (pos == 63)           info.updated(4, false)
-      else                          info
+    if (pos == whiteKingPos) info.updated(2, false).updated(4, false)
+    else if (pos == blackKingPos) info.updated(3, false).updated(5, false)
+    else if (pos == 0) info.updated(3, false)
+    else if (pos == 7) info.updated(5, false)
+    else if (pos == 56) info.updated(2, false)
+    else if (pos == 63) info.updated(4, false)
+    else info
 
     //Checks if white pawn has moved to 8th rank, or black pawn has moved to 1st rank.
     //TODO: Add choice between queen, rook, bishop and knight. Currently, queen is automatically chosen as replacement.
-    if ((row(pos) == 1 || row(pos) == 6) && position(pos).pieceType == 'P'){
+    if ((row(pos) == 1 || row(pos) == 6) && position(pos).pieceType == 'P') {
       if (position(pos).color == 'W') {
         this.copy(position = position
           .updated(pos, Piece("EE"))
@@ -85,20 +91,20 @@ case class Board(position: List[Piece], info: List[Boolean], enPassantCol: Int) 
   }
 
 
-  def generateSuccessor(color: Char) = {
+  def generateSuccessor(color: Char): List[Board] = {
     val moves = Moves(this)
     position
       .zipWithIndex
-      .filter{ case (piece, index) => piece.color == color }
-      .flatMap{ case (piece, index) => moves.getMovesAt(index).map(move => changedBoard(index, move))} ++
+      .filter { case (piece, index) => piece.color == color }
+      .flatMap { case (piece, index) => moves.getMovesAt(index).map(move => changedBoard(index, move)) } ++
       moves.castleMoves(color)
   }
 
   def posFromPiece(piece: Piece): List[Int] = {
     position
       .zipWithIndex
-      .filter{ case (p, index) => p == piece}
-      .map{ case (p, index) => index}
+      .filter { case (p, index) => p == piece }
+      .map { case (p, index) => index }
   }
 
 }
--- src/main/scala-2.12/Evaluation.scala
+++ src/main/scala-2.12/Evaluation.scala
@@ -6,53 +6,53 @@ case class Evaluation(board: Board) {
   val numWhitePawns = board.position.count(piece => piece.prop == "WP")
   val numBlackPawns = board.position.count(piece => piece.prop == "BP")
 
-  def totalHeuristics(): Double = {
+  def totalHeuristics: Double = {
     //material + pawn
-    positionValues + knight + bishop + rook + mobility
+    positionValues + material + knight + bishop + rook + mobility
   }
 
   def material: Double = board.position.foldLeft(0.0)(_ + _.value)
 
   def positionValues = {
     val knightValues = Array(
-        -.50, -.40, -.30, -.30, -.30, -.30, -.40, -.50,
-        -.40, -.20, 0, 0, 0, 0, -.20, -.40,
-        -.30, 0, .10, .15, .15, .10, 0, -.30,
-        -.30, .05, .15, .20, .20, .15, .05, -.30,
-        -.30, 0, .15, .20, .20, .15, 0, -.30,
-        -.30, .05, .10, .15, .15, .10, .05, -.30,
-        -.40, -.20, 0, .05, .05, 0, -.20, -.40,
-        -.50, -.40, -.30, -.30, -.30, -.30, -.40, -.50)
+      -.50, -.40, -.30, -.30, -.30, -.30, -.40, -.50,
+      -.40, -.20, 0, 0, 0, 0, -.20, -.40,
+      -.30, 0, .10, .15, .15, .10, 0, -.30,
+      -.30, .05, .15, .20, .20, .15, .05, -.30,
+      -.30, 0, .15, .20, .20, .15, 0, -.30,
+      -.30, .05, .10, .15, .15, .10, .05, -.30,
+      -.40, -.20, 0, .05, .05, 0, -.20, -.40,
+      -.50, -.40, -.30, -.30, -.30, -.30, -.40, -.50)
 
     val bishopValues = Array(
       -.20, -.10, -.10, -.10, -.10, -.10, -.10, -.20,
-      -.10,   0,    0,    0,    0,    0,    0,  -.10,
-      -.10,   0,  .05,  .10,  .10,  .05,    0,  -.10,
-      -.10, .05,  .05,  .10,  .10,  .05,  .05,  -.10,
-      -.10,   0,  .10,  .10,  .10,  .10,    0,  -.10,
-      -.10, .10,  .10,  .10,  .10,  .10,  .10,  -.10,
-      -.10, .05,    0,    0,    0,    0,  .05,  -.10,
+      -.10, 0, 0, 0, 0, 0, 0, -.10,
+      -.10, 0, .05, .10, .10, .05, 0, -.10,
+      -.10, .05, .05, .10, .10, .05, .05, -.10,
+      -.10, 0, .10, .10, .10, .10, 0, -.10,
+      -.10, .10, .10, .10, .10, .10, .10, -.10,
+      -.10, .05, 0, 0, 0, 0, .05, -.10,
       -.20, -.10, -.10, -.10, -.10, -.10, -.10, -.20)
 
     val rookValues = Array(
-      0,  0,   0,   0,   0,   0,   0,   0,
+      0, 0, 0, 0, 0, 0, 0, 0,
       .05, .10, .10, .10, .10, .10, .10, .05,
-      -.05,  0,   0,   0,   0,   0,   0, -.05,
-      -.05,  0,   0,   0,   0,   0,   0, -.05,
-      -.05,  0,   0,   0,   0,   0,   0, -.05,
-      -.05,  0,   0,   0,   0,   0,   0, -.05,
-      -.05,  0,   0,   0,   0,   0,   0, -.05,
-      0,  0,   0, .05, .05,   0,   0,  0
+      -.05, 0, 0, 0, 0, 0, 0, -.05,
+      -.05, 0, 0, 0, 0, 0, 0, -.05,
+      -.05, 0, 0, 0, 0, 0, 0, -.05,
+      -.05, 0, 0, 0, 0, 0, 0, -.05,
+      -.05, 0, 0, 0, 0, 0, 0, -.05,
+      0, 0, 0, .05, .05, 0, 0, 0
     )
 
     val queenValues = Array(
       -.20, -.10, -.10, -.05, -.05, -.10, -.10, -.20,
-      -.10,  0,  0,  0,  0,  0,  0, -.10,
-      -.10,  0,  .05,  .05,  .05,  .05,  0, -.10,
-      -.05,  0,  .05,  .05,  .05,  .05,  0, -.05,
-      0,  0,  .05,  .05,  .05,  .05,  0, -.05,
-      -.10,  .05,  .05,  .05,  .05,  .05,  0, -.10,
-      -.10,  0,  .05,  0,  0,  0,  0, -.10,
+      -.10, 0, 0, 0, 0, 0, 0, -.10,
+      -.10, 0, .05, .05, .05, .05, 0, -.10,
+      -.05, 0, .05, .05, .05, .05, 0, -.05,
+      0, 0, .05, .05, .05, .05, 0, -.05,
+      -.10, .05, .05, .05, .05, .05, 0, -.10,
+      -.10, 0, .05, 0, 0, 0, 0, -.10,
       -.20, -.10, -.10, -.05, -.05, -.10, -.10, -.20
     )
 
@@ -63,19 +63,19 @@ case class Evaluation(board: Board) {
       -.30, -.40, -.40, -.50, -.50, -.40, -.40, -.30,
       -.20, -.30, -.30, -.40, -.40, -.30, -.30, -.20,
       -.10, -.20, -.20, -.20, -.20, -.20, -.20, -.10,
-        .20, .20,  0,  0,  0,  0, .20, .20,
-        .20, .30, .10,  0,  0, .10, .30, .20
+      .20, .20, 0, 0, 0, 0, .20, .20,
+      .20, .30, .10, 0, 0, .10, .30, .20
     )
 
     val kingValuesLate = Array(
-       -.50, -.40, -.30, -.20, -.20, -.30, -.40, -.50,
-       -.30, -.20, -.10,  0,  0, -.10, -.20, -.30,
-       -.30, -.10, .20, .30, .30, .20, -.10, -.30,
-       -.30, -.10, .30, .40, .40, .30, -.10, -.30,
-       -.30, -.10, .30, .40, .40, .30, -.10, -.30,
-       -.30, -.10, .20, .30, .30, .20, -.10, -.30,
-       -.30, -.30,  0,  0,  0,  0, -.30, -.30,
-       -.50, -.30, -.30, -.30, -.30, -.30, -.30, -.50
+      -.50, -.40, -.30, -.20, -.20, -.30, -.40, -.50,
+      -.30, -.20, -.10, 0, 0, -.10, -.20, -.30,
+      -.30, -.10, .20, .30, .30, .20, -.10, -.30,
+      -.30, -.10, .30, .40, .40, .30, -.10, -.30,
+      -.30, -.10, .30, .40, .40, .30, -.10, -.30,
+      -.30, -.10, .20, .30, .30, .20, -.10, -.30,
+      -.30, -.30, 0, 0, 0, 0, -.30, -.30,
+      -.50, -.30, -.30, -.30, -.30, -.30, -.30, -.50
     )
 
     board.position.zipWithIndex
@@ -91,7 +91,7 @@ case class Evaluation(board: Board) {
           case "BR" => -rookValues.reverse(index)
           case "BK" => -kingValuesEarly.reverse(index)
           case "BQ" => -queenValues.reverse(index)
-          case _    => 0
+          case _ => 0
         }
       }
       .sum
@@ -111,9 +111,9 @@ case class Evaluation(board: Board) {
         val cols = board.positionAsCols()
         val pawn = if (color == 'W') Piece("WP") else Piece("BP")
         (for (i <- 0 until 8) yield {
-          if (cols(i).contains(pawn)){
+          if (cols(i).contains(pawn)) {
             if (prevContainsPawn) 0
-            else{
+            else {
               prevContainsPawn = true
               1
             }
@@ -127,15 +127,15 @@ case class Evaluation(board: Board) {
       numOfIslands('B') - numOfIslands('W')
     }
 
-    0.4*doublePawn + 0.2*pawnIslands
+    0.4 * doublePawn + 0.2 * pawnIslands
   }
 
   def knight = {
     def decreasingWithFewerPawns = {
-      ((numWhitePawns + numBlackPawns)/16)*
+      ((numWhitePawns + numBlackPawns) / 16) *
         (board.position.count(piece => piece.prop == "WN") - board.position.count(piece => piece.prop == "BN"))
     }
-    0.2*decreasingWithFewerPawns
+    0.2 * decreasingWithFewerPawns
   }
 
   def bishop = {
@@ -148,21 +148,21 @@ case class Evaluation(board: Board) {
       whiteBishopPair - blackBishopPair
     }
     def fianchetto = {
-      val whiteLeft   = if (board.position(49) == Piece("WB")) 1 else 0
-      val whiteRight  = if (board.position(54) == Piece("WB")) 1 else 0
-      val blackLeft   = if (board.position(9)  == Piece("WB")) 1 else 0
-      val blackRight  = if (board.position(14) == Piece("WB")) 1 else 0
+      val whiteLeft = if (board.position(49) == Piece("WB")) 1 else 0
+      val whiteRight = if (board.position(54) == Piece("WB")) 1 else 0
+      val blackLeft = if (board.position(9) == Piece("WB")) 1 else 0
+      val blackRight = if (board.position(14) == Piece("WB")) 1 else 0
       whiteLeft + whiteRight - blackRight - blackRight
     }
     def badBishop = ???
-    pair + 0.2*fianchetto
+    pair + 0.2 * fianchetto
   }
 
   def rook = {
     val whiteRooks = board.posFromPiece(Piece("WR"))
     val blackRooks = board.posFromPiece(Piece("BR"))
     def increasingWithFewerPawns = {
-      ((16 - numWhitePawns - numBlackPawns)/16)*
+      ((16 - numWhitePawns - numBlackPawns) / 16) *
         (board.position.count(piece => piece.prop == "WR") - board.position.count(piece => piece.prop == "BR"))
     }
     def seventhRank = {
@@ -180,8 +180,8 @@ case class Evaluation(board: Board) {
       val passedPawnsWhite = whitePawnsRows diff blackPawnsRows
       val passedPawnsBlack = blackPawnsRows diff whitePawnsRows
 
-      whiteRooks.count{ pos => passedPawnsWhite.contains(board.row(pos)) } -
-        blackRooks.count{ pos => passedPawnsBlack.contains(board.row(pos))}
+      whiteRooks.count { pos => passedPawnsWhite.contains(board.row(pos)) } -
+        blackRooks.count { pos => passedPawnsBlack.contains(board.row(pos)) }
     }
 
     //Give small bonus for having rook on same file as enemy queen
@@ -193,7 +193,7 @@ case class Evaluation(board: Board) {
 
     }
 
-    0.3*increasingWithFewerPawns + 0.3*seventhRank + 0.2*tarraschRule + 0.1*enemyQueenOnSameFile
+    0.3 * increasingWithFewerPawns + 0.3 * seventhRank + 0.2 * tarraschRule + 0.1 * enemyQueenOnSameFile
   }
 
   def mobility = {
